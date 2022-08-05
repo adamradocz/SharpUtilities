f779212fc9566745c7a4a07d9ca39972698c1bef
Adam Radocz <adam.radocz@gmail.com>
2022-08-05 10:12:03 +0200

    Register Configure with ConfigureWritable

--- SharpUtilities/ServiceCollectionExtensions.cs
+++ SharpUtilities/ServiceCollectionExtensions.cs
@@ -19,18 +19,19 @@ public static class ServiceCollectionExtensions
 
     public static IServiceCollection ConfigureWritable<TOptions>(this IServiceCollection services, IConfigurationSection section) where TOptions : class
     {
-        return services.AddSingleton<IWritableOptionsMonitor<TOptions>>(serviceProvider =>
-        {
-            var optionsFactory = serviceProvider.GetRequiredService<IOptionsFactory<TOptions>>();
-            var optionsChangeTokenSources = serviceProvider.GetRequiredService<IEnumerable<IOptionsChangeTokenSource<TOptions>>>();
-            var optionsMonitorCache = serviceProvider.GetRequiredService<IOptionsMonitorCache<TOptions>>();
-            var options = serviceProvider.GetRequiredService<IOptions<WritableOptionsMonitorOption>>();
-            var hostEnvironment = serviceProvider.GetRequiredService<IHostEnvironment>();
-            var configurationRoot = (IConfigurationRoot)serviceProvider.GetRequiredService<IConfiguration>();
-            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
-            var logger = loggerFactory.CreateLogger<TOptions>();
-            return new WritableOptionsMonitor<TOptions>(optionsFactory, optionsChangeTokenSources, optionsMonitorCache, options, hostEnvironment, configurationRoot, section, logger);
-        });
+        return services.Configure<TOptions>(section)
+            .AddSingleton<IWritableOptionsMonitor<TOptions>>(serviceProvider =>
+            {
+                var optionsFactory = serviceProvider.GetRequiredService<IOptionsFactory<TOptions>>();
+                var optionsChangeTokenSources = serviceProvider.GetRequiredService<IEnumerable<IOptionsChangeTokenSource<TOptions>>>();
+                var optionsMonitorCache = serviceProvider.GetRequiredService<IOptionsMonitorCache<TOptions>>();
+                var options = serviceProvider.GetRequiredService<IOptions<WritableOptionsMonitorOption>>();
+                var hostEnvironment = serviceProvider.GetRequiredService<IHostEnvironment>();
+                var configurationRoot = (IConfigurationRoot)serviceProvider.GetRequiredService<IConfiguration>();
+                var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+                var logger = loggerFactory.CreateLogger<TOptions>();
+                return new WritableOptionsMonitor<TOptions>(optionsFactory, optionsChangeTokenSources, optionsMonitorCache, options, hostEnvironment, configurationRoot, section, logger);
+            });
     }
 
     public static IServiceCollection AddGenericServices(this IServiceCollection services)
